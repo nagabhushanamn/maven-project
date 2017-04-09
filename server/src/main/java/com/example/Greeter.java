e525e3e65c91392d71c0a66d0c9d91b265e4aa37
NAG <nagabhushanamn@gmail.com>
2017-04-09 22:47:48 +0530

    fixed all warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,11 +13,10 @@ public class Greeter {
   }
 
   /**
-   * This is greet method
    * @param someone the name of person
    * @return greeting string
    */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
